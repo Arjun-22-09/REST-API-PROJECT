2d4110f5f8da787c2c39033092eb35ec30b4f90c
Arjun-22-09 <727723eucd004@skcet.ac.in>
2025-04-02 17:42:42 +0530

    Final commit

--- springapp/src/main/java/com/arjun/springapp/Repository/EventRepository.java
+++ springapp/src/main/java/com/arjun/springapp/Repository/EventRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.arjun.springapp.Entity.event;
 
 public interface EventRepository extends JpaRepository<event, Integer> {
+    
 }
